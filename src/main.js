7866ab9e84d43f31b70c5a82cc4a4912a02f5174
async <yuanlb@yczcjk.com>
2020-11-19 17:30:25 +0800

    -feat [package] add modules

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue';
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Antd);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app');
