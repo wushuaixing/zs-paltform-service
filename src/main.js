0e3d3bd6bf580464a1ba7e6689cd18763c79d982
async-6441 <yuanlb@yczcjk.com>
2020-12-08 14:15:06 +0800

    -feat [version] add version info

--- src/main.js
+++ src/main.js
@@ -9,6 +9,14 @@ Vue.config.productionTip = false;
 Vue.use(Antd);
 Vue.prototype.$form = Form
 
+const Version = 'v1.0.0';
+const BetaNumber = '.1';
+const info = `Version：${Version}${BetaNumber ? `-beta${BetaNumber}` : ''}`;
+window.CurrentVersions = info;
+if (window.location.protocol === 'http:') {
+  console.info(info);
+}
+
 new Vue({
   router,
   store,
