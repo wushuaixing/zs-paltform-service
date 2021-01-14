ab867f1c49415686e65f87466e247ef94ad9d31d
metotna <me-totna@foxmail.com>
2021-01-15 02:13:36 +0800

    -fix [version] v1.0.0-beta.13

--- src/version.js
+++ src/version.js
@@ -2,7 +2,7 @@
  * 当前版本信息
  */
 const Version = 'v1.0.0';
-const BetaNumber = '.12';
+const BetaNumber = '.13';
 const info = `Version「S」：${Version}${BetaNumber ? `-beta${BetaNumber}` : ''}`;
 window.CurrentVersions = info;
 if (window.location.protocol === 'http:') {
